9a2eb76942a1e7227ecc07439d7f500510240ab7
“malcolmmutare” <mutarem@gmail.com>
2023-12-04 14:27:15 +0200

    added media query to logo

--- src/routes/home/navigation/navigation.styles.jsx
+++ src/routes/home/navigation/navigation.styles.jsx
@@ -14,6 +14,17 @@ export const LogoContainer = styled(Link)`
     height: 100%; 
     width: 70px;  
     padding: 25px;
+
+    @media screen and (max-width: 550px) {
+      width: 180px;
+      
+      margin-top: 10px;
+  
+    }
+  
+    @media screen and (max-width: 400px) {
+      width: 130px;
+    }
     `;
 
 export const NavLinks = styled.div`
