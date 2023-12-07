16501a71db3e990e1909576f281dc46a187b52dc
“malcolmmutare” <mutarem@gmail.com>
2023-12-07 16:44:17 +0200

    clean-up cleaning up oustanding styling elements, as well as polishup responsiveness of pages

--- src/routes/category/category.styles.jsx
+++ src/routes/category/category.styles.jsx
@@ -1,11 +1,36 @@
 import styled from 'styled-components';
 
+export const PageWrapper = styled.div`
+  /* margin-left: 15px;
+  margin-right: 15px; */
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  justify-content: space-between;
+`;
+
 export const CategoryContainer = styled.div`
+    color: #89cffa;
     display: grid;
     grid-template-columns: repeat(4, 1fr);
     column-gap: 20px;
     row-gap: 50px;
-    `
+
+    padding-top: 15px;
+  padding-bottom: 15px;
+
+  @media (max-width: 800px) {
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 20px;
+    row-gap: 20px;
+  }
+  @media (max-width: 500px) {
+    display: flex;
+    flex-direction: column;
+    column-gap: 10px;
+    row-gap: 10px;
+  }
+    `;
 
 
 export const CategoryTitle = styled.h2`
@@ -13,4 +38,8 @@ export const CategoryTitle = styled.h2`
     font-size: 38px;
     margin-bottom: 25px;
     text-align: center;
-`
+`;
+
+export const FooterWrapper = styled.div`
+  margin-top: auto;
+`;
